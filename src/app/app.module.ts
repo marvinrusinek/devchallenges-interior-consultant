8f6a3cc19466a3e0b06fa74e517a4a323ae779e2
Marvin Rusinek <marvinrusinek@gmail.com>
2020-12-06 17:31:47 -0500

    initial commit
    interior consultant webpage from DevChallenges, coded using Angular 11, Bootstrap, and SCSS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContainerComponent } from './interior/containers/container/container.component';
+import { HeaderComponent } from './interior/containers/header/header.component';
+import { MainComponent } from './interior/containers/main/main.component';
+import { FooterComponent } from './interior/containers/footer/footer.component';
+
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'interior',
+    pathMatch: 'full'
+  },
+  {
+    path: 'interior',
+    loadChildren: () =>
+      import('./interior/routing/interior-routing.module').then(m => m.InteriorRoutingModule)
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    FooterComponent,
+    ContainerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
